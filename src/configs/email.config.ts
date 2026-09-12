c101153e0e7bb315690461a630b6cf3ec39258bf
Abduselam Awel <abdselm676@gmail.com>
2026-09-12 19:42:41 +0300

    fix the email config

--- src/configs/email.config.ts
+++ src/configs/email.config.ts
@@ -2,7 +2,9 @@ import nodemailer from "nodemailer";
 import ENV from "../utils/env";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: ENV.EMAIL_USER,
     pass: ENV.EMAIL_PASSWORD,
